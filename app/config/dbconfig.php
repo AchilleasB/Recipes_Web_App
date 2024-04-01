2dbc75129021bed5a5ebc30a84e8f066dcf4b0fc
AchilleasB <90069683+AchilleasB@users.noreply.github.com>
2024-04-01 11:28:43 +0200

    Update dbconfig.php

--- app/config/dbconfig.php
+++ app/config/dbconfig.php
@@ -1,6 +1,11 @@
 <?php
-$servername = "mysql";
-$username = "root";
-$password = "secret123";
-$database = "web_recipes";
-?>
+// $servername = "mysql";
+// $username = "root";
+// $password = "secret123";
+// $database = "web_recipes";
+// ?>
+
+$servername = getenv('DB_SERVERNAME');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
